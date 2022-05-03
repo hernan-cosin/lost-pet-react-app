ed5bf80b22380abd3a62525c36d61df3b01bf32c
hernan-cosin <78047711+hernan-cosin@users.noreply.github.com>
2022-05-03 12:01:12 -0300

    modified webpack scripts

---
+++ src/components/mapbox-search-form/index.tsx
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+const Map = ReactMapboxGl({
+  accessToken:
+    "pk.eyJ1IjoiaGFjaGVjb3NpbiIsImEiOiJja3pwaXd1amg2OTE2MndtemtmMHV4MzN1In0.D0p6cNSXOMBxio-BgB6gbA",
+});
+
+const boxStyles = {
+  padding: 10,
+  fontSize: 20,
+};
+
+type MapBoxSearchProps = {
+  onChange?: (any) => any;
+};
+
+function MapboxSearch(props: MapBoxSearchProps) {
+  const { onChange } = props;
+  const [query, setQuery] = useState("");
+  // lo seteo any porque la prop "center" de Map se queja
+  const initialCoords: any = [-58.4100942,-34.605681];
+  const [coords, setCoords] = useState(initialCoords);
+
+  async function search() {
+    // esta API la saqué de por ahi
+    const data = await fetch(
+      `https://us1.locationiq.com/v1/search.php?key=pk.bf4604bc2b3ea328e732de26a4387fa9&q=${query}&format=json`
+    ).then((r) => r.json());
+    console.log(data);
+    const lat = parseFloat(data[0].lat);
+    const lon = parseFloat(data[0].lon);
+    const newCoords = [lon, lat];
+    setCoords(newCoords);
+
+    // lo "tiro" hacia arriba para que reciban las coordenadas desde "afuera"
+    if (onChange) {
+      onChange({
+        query: query,
+        coords: newCoords,
+      });
+    }
+  }
+
+  function inputChangeHandler(e) {      
+    setQuery(e.target.value);    
+  }
+
+  function keydownInputHandler(e) {
+    // si no es con form, tengo que agregar esto
+    if (e.key == "Enter") {
+      // evito que se dispare el submit
+      e.preventDefault();      
+      search();
+    }
+  }
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          onChange={inputChangeHandler}
+          onKeyDown={keydownInputHandler}
+          value={query}
+          style={boxStyles}
+        />
+        <button style={boxStyles} onClick={search} type="button">
+          Buscar
+        </button>
+      </div>
+      <Map
+        style="mapbox://styles/mapbox/streets-v9"
+        containerStyle={{
+          height: "350px",
+          width: "350px",
+          borderRadius: "4px",
+          margin: "10px auto 0 auto" 
+        }}
+        zoom={[11]}
+        center={coords}
+        movingMethod="easeTo"
+      >
+        <Layer type="symbol" id="marker" layout={{ "icon-image": "marker-15" }}>
+          <Feature coordinates={coords} />
+        </Layer>
+      </Map>
+    </div>
+  );
+}
+
+export { MapboxSearch };
--- src/components/report-pet-form/index.tsx
+++ src/components/report-pet-form/index.tsx
@@ -3,11 +3,13 @@ import css from "./index.css"
 import {Button} from "ui/buttons"
 import {SearchInput} from "ui/text-field"
 import {AreaInput} from "ui/text-field/text-area"
+import {MapboxSearch} from "components/mapbox-search-form"
 
 export function ReportPetForm() {
     return <>
         <SearchInput name="name" type="text" label="Nombre"></SearchInput>
         <AreaInput name="description" label="Descripción"></AreaInput>
+        <MapboxSearch/>
         <Button color="yellow">Reportar como perdido</Button>
     </>
 }
